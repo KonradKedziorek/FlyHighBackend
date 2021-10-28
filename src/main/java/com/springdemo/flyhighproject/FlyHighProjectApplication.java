46c161422f9a85b91fb2d90f50dbdfc1131d38b7
Gabriel <holubowicz.g@gmail.com>
2021-10-28 17:57:22 +0200

    Initial Commit

---
+++ src/main/java/com/springdemo/flyhighproject/FlyHighProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springdemo.flyhighproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyHighProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlyHighProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springdemo/flyhighproject/FlyHighProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdemo.flyhighproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyHighProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
