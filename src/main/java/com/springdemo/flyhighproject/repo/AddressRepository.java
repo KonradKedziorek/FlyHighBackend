0cef7dbf8f4507ccaab1fe4327d8ab7b007fb935
Dawid Iwaniuk <dawuso@gmail.com>
2021-12-09 12:48:04 +0100

    Podstawowe metody w repo

--- src/main/java/com/springdemo/flyhighproject/repo/AddressRepository.java
+++ src/main/java/com/springdemo/flyhighproject/repo/AddressRepository.java
@@ -6,4 +6,16 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
+    Address findById(long id);
+    Address findAllByCity(String city);
+    Address findAllByCountry(String country);
+    Address findAllByStreet(String street);
+    Address findAllByHouseNumber(String houseNumber);
+    Address findAllByZipCode(String zipCode);
+    Address deleteById(long id);
+    Address deleteAllByCity(String city);
+    Address deleteAllByCountry(String country);
+    Address deleteAllByStreet(String street);
+    Address deleteAllByHouseNumber(String houseNumber);
+    Address deleteAllByZipCode(String zipCode);
 }
--- src/main/java/com/springdemo/flyhighproject/repo/UserRepository.java
+++ src/main/java/com/springdemo/flyhighproject/repo/UserRepository.java
@@ -6,4 +6,17 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findById(long id);
+    User findByPesel(long pesel);
+    User findAllByName(String name);
+    User findAllByMiddleName(String middleName);
+    User findAllBySurname(String surname);
+    User findByBankAccount(long bankAccount);
+    User deleteByPesel(long pesel);
+    User deleteById(long id);
+    User deleteAllByName(String name);
+    User deleteAllByMiddleName(String middleName);
+    User deleteAllBySurname(String surname);
+    User deleteByBankAccount(long bankAccount);
+
 }
