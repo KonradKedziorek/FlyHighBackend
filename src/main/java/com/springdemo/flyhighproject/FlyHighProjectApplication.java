0b244ffc67637e361acd2386436ad0ab8be0e8a2
Dawid Iwaniuk <dawuso@gmail.com>
2022-03-21 16:05:23 +0100

    Połączenie z api, ale bez wyświetlania danych

--- src/main/java/com/springdemo/flyhighproject/FlyHighProjectApplication.java
+++ src/main/java/com/springdemo/flyhighproject/FlyHighProjectApplication.java
@@ -8,12 +8,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 @EntityScan
 @SpringBootApplication
 public class FlyHighProjectApplication implements CommandLineRunner {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(FlyHighProjectApplication.class, args);
+
+        URL url = new URL("https://api.duffel.com/air/airports?limit=5");
+        HttpURLConnection http = (HttpURLConnection)url.openConnection();
+        http.setRequestProperty("Accept-Encoding", "gzip");
+        http.setRequestProperty("Accept", "application/json");
+        http.setRequestProperty("Duffel-Version", "beta");
+        http.setRequestProperty("Authorization", "Bearer duffel_test_Rmi5mLsXEIzudveaH67TScVC0zkc6HlyIucDww7RQ0P");
+
+        System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
+        http.disconnect();
+
     }
 
     @Override
