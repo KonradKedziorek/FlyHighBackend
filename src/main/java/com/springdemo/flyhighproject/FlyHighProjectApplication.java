a7b719bab5a33b3dff136c3fd0754e64f97d4faa
Dawid Iwaniuk <dawuso@gmail.com>
2022-03-26 20:11:43 +0100

    Wyświetlanie danych jsona

--- src/main/java/com/springdemo/flyhighproject/FlyHighProjectApplication.java
+++ src/main/java/com/springdemo/flyhighproject/FlyHighProjectApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -22,12 +24,21 @@ public class FlyHighProjectApplication implements CommandLineRunner {
 
         URL url = new URL("https://api.duffel.com/air/airports?limit=5");
         HttpURLConnection http = (HttpURLConnection)url.openConnection();
-        http.setRequestProperty("Accept-Encoding", "gzip");
+        http.setRequestMethod("GET");
+        //http.setRequestProperty("Accept-Encoding", "gzip"); ten header zwraca skompresowane dane, jeżeli będziemy wiedzieli jak je zdekomprsować i chcieli to można go przywrócić
         http.setRequestProperty("Accept", "application/json");
         http.setRequestProperty("Duffel-Version", "beta");
         http.setRequestProperty("Authorization", "Bearer duffel_test_Rmi5mLsXEIzudveaH67TScVC0zkc6HlyIucDww7RQ0P");
-
+        http.getInputStream();
+        http.connect();
         System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
+
+        final BufferedReader in = new BufferedReader(new InputStreamReader(http.getInputStream()));
+        String line;
+        while ((line = in.readLine()) != null) {
+            System.out.println(line);
+        }
+
         http.disconnect();
 
     }
